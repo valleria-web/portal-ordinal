4bbae34a8fc23904d0b3ec0c8497b565535df6e9
valleria-web <valleria@gmail.com>
2023-03-12 00:13:33 +0100

    markdown

--- app/api/hello/posts/[slug]/page.tsx
+++ app/api/hello/posts/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import fs from "fs";
 import Markdown from "markdown-to-jsx";
 import matter from "gray-matter";
-import getPostMetadata from "@/components/getPostMetadata";
+import getPostMetadata from "@/app/components/getPostMetadata";
 
 const getPostContent = (slug: string) => {
   const folder = "public/posts/";
